5489d279a4f960327d3a3f45adda358aa0bd18cc
张良 <1077167261@qq.com>
2024-07-09 16:16:35 +0800

    初始化readme 以及栈的应用

--- 1.栈结构的封装.js
+++ 1.栈结构的封装.js
@@ -39,13 +39,31 @@ class Stack {
   }
 }
 let stack = new Stack()
-stack.push(7)
-stack.push(8)
-stack.push(9)
-console.log(stack.peek());
-console.log(stack.isEmpty());
-console.log(stack.size());
-console.log(stack.toString());
-stack.clear()
 
+// 栈结构的应用 十进制转任意进制
+function convert(decNumber,base){
+  let remstack = new Stack()
+  let number = decNumber
+  let str = ''
+  let basestr ='0123456789ABCDEF'
+  // 使用辗转相除法
+  while(number>0){
+    remstack.push(number%base)
+    number =Math.floor(number/base) 
+  }
+  while(!(remstack.isEmpty())){
+     str+= basestr[remstack.pop()]
+  }
+  return str
+}
+// stack.push(7)
+// stack.push(8)
+// stack.push(9)
+// console.log(stack.peek());
+// console.log(stack.isEmpty());
+// console.log(stack.size());
+// console.log(stack.toString());
+// stack.clear()
+
+console.log(convert(49,8));
 
